7b66cb93d7d9489764cb470590a94a9608270b3f
Lephise <meikir@hotmail.com>
2013-10-24 10:21:08 +0300

    Added tuntiharjoitukset 24.10

---
+++ MyRegEx.aspx
@@ -0,0 +1,18 @@
+﻿<%@ Page Title="RegexiStuffaa" Language="C#" MasterPageFile="~/MasterPage.master" AutoEventWireup="true" CodeFile="MyRegEx.aspx.cs" Inherits="MyRegEx" theme="Kaunis"%>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="head" Runat="Server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" Runat="Server">
+    <h1>Regular Expression testi</h1>
+    <h2>Kirjoita oheiseen text boksiin jotakin</h2>
+    <br />Name plox: <br />
+    <asp:TextBox ID="txt1" runat="server"></asp:TextBox>
+    <asp:Button ID="btn1" runat="server" Text="Validate" OnClick="btn1_Click" style="height: 26px" /> <br />
+    <asp:Label ID="lbl1" runat="server"></asp:Label>
+
+    <br />Anna eemaili: <br />
+    <asp:TextBox ID="txt2" runat="server"></asp:TextBox>
+    <asp:Button ID="btn2" runat="server" Text="Validate" style="height: 26px" OnClick="btn2_Click" /> <br />
+    <asp:Label ID="lbl2" runat="server"></asp:Label>
+</asp:Content>
+
---
+++ MyRegEx.aspx.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class MyRegEx : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+    protected void btn1_Click(object sender, EventArgs e)
+    {
+        String regex = "^[a-zA-Z]*$";
+        String st = txt1.Text;
+
+        if(Regex.IsMatch(st, regex)){
+            lbl1.Text = "Kelpaa";
+        }
+        else{
+            lbl1.Text = "Ei kelpaa";
+        }
+
+    }
+    protected void btn2_Click(object sender, EventArgs e)
+    {
+        String regex = @"^([\w-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([\w-]+\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$";
+           // "^[a-zA-Z]*$";
+        String st = txt2.Text;
+
+        if (Regex.IsMatch(st, regex))
+        {
+            lbl1.Text = "Kelpaa";
+        }
+        else
+        {
+            lbl1.Text = "Ei kelpaa";
+        }
+    }
+}
