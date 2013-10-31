e337a9db57b30d201c73dc78d76658b7e0ea3131
Lephise <meikir@hotmail.com>
2013-10-31 11:09:13 +0200

    Listty vkharjoitukset

---
+++ f6689_haeilmot.aspx
@@ -0,0 +1,22 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/MasterPage.master" AutoEventWireup="true" CodeFile="f6689_haeilmot.aspx.cs" Inherits="f6689_haeilmot" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="head" Runat="Server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" Runat="Server">
+
+    <asp:SqlDataSource runat="server" ID="srcIlm" ConnectionString="Data Source=eight.labranet.jamk.fi;Initial Catalog=DemoxOy;Persist Security Info=True;User ID=koodari;Password=koodari13"
+        SelectCommand="SELECT asioid, lastname, firstname, date FROM lasnaolot WHERE course like 'IIO13200%' ORDER BY asioid"></asp:SqlDataSource>
+
+    <h1>Läsnäolot</h1>
+
+    Sukunimi:<asp:TextBox ID="txt" runat="server"></asp:TextBox><br />
+    Opintojakso:<asp:DropDownList ID="drop" runat="server"></asp:DropDownList><br />
+    <asp:Button ID="btn" runat="server" Text="Hae" OnClick="btn_Click" />
+
+    <asp:RegularExpressionValidator ID="regex" runat="server" ValidationExpression="^[a-öA-Ö]*$" ErrorMessage="Sukunimi voi sisältää vain kirjaimia" ControlToValidate="txt"></asp:RegularExpressionValidator>
+
+    
+    <asp:GridView ID="grid" runat="server"></asp:GridView>
+    
+</asp:Content>
+
---
+++ f6689_haeilmot.aspx.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+
+public partial class f6689_haeilmot : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+        if (!IsPostBack)
+        {
+            /*
+            VAIHTOEHTOINEN TAPA
+             
+            SqlConnection cn = new SqlConnection();
+
+            cn.ConnectionString = "Data Source=eight.labranet.jamk.fi;Initial Catalog=DemoxOy;Persist Security Info=True;User ID=koodari;Password=koodari13";
+
+            SqlCommand cm = new SqlCommand("SELECT DISTINCT(course) FROM lasnaolot");
+
+            cm.Connection = cn;
+            cn.Open();
+            
+            drop.DataSource = cm.ExecuteReader();
+             */
+
+            srcIlm.SelectCommand = "SELECT DISTINCT(course) FROM lasnaolot";
+            
+            drop.DataSource = srcIlm;
+            drop.DataTextField = "course";
+            drop.DataBind();
+        }
+    }
+
+    protected void btn_Click(object sender, EventArgs e)
+    {
+        if (txt.Text != null)
+        {
+            srcIlm.SelectCommand = "SELECT asioid, lastname, firstname, date FROM lasnaolot WHERE course like '"+drop.SelectedItem.Text+"%' AND lastname like '" + txt.Text + "%' ORDER BY asioid";
+            grid.DataSource = srcIlm;
+            grid.DataBind();
+        }
+
+    }
+}
