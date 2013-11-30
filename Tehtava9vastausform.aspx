3520e4ac02c2ed935c0773140a5a4e5dea8b2ede
Lephise <meikir@hotmail.com>
2013-11-30 14:06:48 +0200

    Tehtava 9 follow up

--- Tehtava9vastausform.aspx
+++ Tehtava9vastausform.aspx
@@ -3,9 +3,13 @@
 <asp:Content ID="Content1" ContentPlaceHolderID="head" Runat="Server">
 </asp:Content>
 <asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" Runat="Server">
+    <h1>Kymysys</h1>
     <asp:Label ID="label" runat="server"></asp:Label>
-    <asp:Panel id="vaihtoehdot" runat="server" />
-    <asp:Button ID="btn" runat="server" OnClick="btn_Click" />
+   
+
+    <asp:RadioButtonList ID="buttoset" runat="server"></asp:RadioButtonList>
+    <asp:Button ID="btn" runat="server" OnClick="btn_Click" Text="Vastaa"/> <br />
+    <asp:Label ID="vastmaara" runat="server"></asp:Label>
     
 </asp:Content>
 
--- Tehtava9vastausform.aspx.cs
+++ Tehtava9vastausform.aspx.cs
@@ -11,48 +11,75 @@ public partial class Tehtava9vastausform : System.Web.UI.Page
 {
     private int[] vastattu = { 0, 0, 0, 0, 0 };
     private int maara;
+  
 
     protected void Page_Load(object sender, EventArgs e)
     {
 
-
-        XmlDocument xmldoc = new XmlDocument();
-        xmldoc.Load(Server.MapPath("~/App_Data/kysymykset.xml"));
-        maara = Convert.ToInt32(xmldoc.SelectSingleNode("/kysymykset/kysymys/maara").InnerText);
-        label.Text = xmldoc.SelectSingleNode("/kysymykset/kysymys/text").InnerText;
-
-        if (maara > 2)
+        if (!IsPostBack)
         {
 
-            /*  RadioButton radio = new RadioButton();
-              radio.Text = xmldoc.SelectSingleNode("/kysymykset/kysymys/vaihtotehdot/vaihtoehto").InnerText;
-              vaihtoehdot.Controls.Add(radio);*/
+            XmlDocument xmldoc = new XmlDocument();
+            xmldoc.Load(Server.MapPath("~/App_Data/kysymykset.xml"));
+            maara = Convert.ToInt32(xmldoc.SelectSingleNode("/kysymykset/kysymys/maara").InnerText);
+            label.Text = xmldoc.SelectSingleNode("/kysymykset/kysymys/text").InnerText;
 
-            XmlNodeList vaihtoehtoja = xmldoc.SelectNodes("/kysymykset/kysymys/vaihtoehdot");
-
-            for (int i = 1; i < maara + 1; i++)
+            if (maara > 2)
             {
 
+                /*  RadioButton radio = new RadioButton();
+                  radio.Text = xmldoc.SelectSingleNode("/kysymykset/kysymys/vaihtotehdot/vaihtoehto").InnerText;
+                  vaihtoehdot.Controls.Add(radio);*/
 
-                RadioButton rad = new RadioButton();
-                rad.ID = "rad" + i.ToString();
-                rad.Text = xmldoc.SelectSingleNode("/kysymykset/kysymys/vaihtotehto[" + i + "]").InnerText;
-                vaihtoehdot.Controls.Add(rad);
+                XmlNodeList vaihtoehtoja = xmldoc.SelectNodes("/kysymykset/kysymys/vaihtoehdot");
+
+                for (int i = 1; i < maara + 1; i++)
+                {
+
+
+                    //RadioButton rad = new RadioButton();
+                    ListItem rad = new ListItem();
+                    //rad.ID = "rad" + i.ToString();
+                    rad.Text = xmldoc.SelectSingleNode("/kysymykset/kysymys/vaihtotehto[" + i + "]/vaiht").InnerText;
+
+                    buttoset.Items.Add(rad);
+                    //vaihtoehdot.Controls.Add(rad);
+                    //buttonit.SetValue(rad, i-1);
+
+
+
+                }
 
 
 
 
             }
-
-
-
-
         }
     }
 
     protected void btn_Click(object sender, EventArgs e)
     {
+        
+        if (buttoset.Items.Count != 0)
+        {
+            XmlDocument xmldoc = new XmlDocument();
+            xmldoc.Load(Server.MapPath("~/App_Data/kysymykset.xml"));
+            XmlNode node = xmldoc.SelectSingleNode("/kysymykset/kysymys/vaihtotehto[" + (buttoset.SelectedIndex + 1) + "]/vastattu");
+            node.InnerText = (Convert.ToInt32(node.InnerText) + 1).ToString();
 
+            
+           // vastattu[buttoset.SelectedIndex]++ ;
+            xmldoc.Save(Server.MapPath("~/App_Data/kysymykset.xml"));
+            
+            label.Text = node.InnerText;
+
+        }
+
+        else
+        {
+            label.Text = "EIJOLE";
+        }
+        
     }
 
 }
